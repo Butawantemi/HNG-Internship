634fdb6e5e45eea143d0cf6dcf61864f102cf9b9
Butawantemi <japhetbuta@gmail.com>
2024-07-01 17:00:14 +0300

    Starting server

---
+++ basic-web-server/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
